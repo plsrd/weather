b06412a091b3d2e18a3a48369c5398c49643675e
racheallarimer <racheal.larimer@gmail.com>
2021-02-03 15:26:45 -0800

    add getLocation and updateDisplay functions

--- src/index.js
+++ src/index.js
@@ -8,10 +8,35 @@ async function getLocation(loc){
   try {
     const response = await fetch(url, {mode: 'cors'});
     const data = await response.json();
-    console.log(data.name)
+    console.log(data);
+    updateDisplay(data);
   } catch {
     console.log('no such city ya tit')
   }
+}
+
+function updateDisplay(data) {
+  const location = document.querySelector('.location');
+  location.textContent = data.name;
+
+  const description = document.querySelector('.description');
+  description.textContent = data.weather[0].description;
+
+  const temp = document.querySelector('.temp');
+  temp.textContent = convertTemp(data.main.temp);
+
+  const feelsLike = document.querySelector('.feels-like');
+  feelsLike.textContent = `${convertTemp(data.main.feels_like)}°`;
+
+  const wind = document.querySelector('.wind');
+  wind.textContent = `${data.wind.speed} mph`;
+
+  const humidity = document.querySelector('.humidity');
+  humidity.textContent = `${data.main.humidity}%`;
+}
+
+function convertTemp(kel) {
+  return Math.round((kel * (9/5)) - 459.67);
 }
 
 
